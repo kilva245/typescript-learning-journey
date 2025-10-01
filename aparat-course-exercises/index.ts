ab19a4a5aba1acf4f10bbe8c07b65a7457bc7ff9
kilva245 <139843031+kilva245@users.noreply.github.com>
2025-10-01 09:09:06 +0330

    data types
    i learned data types and how to not use them at incorrect ways :)

--- aparat-course-exercises/index.ts
+++ aparat-course-exercises/index.ts
@@ -1,2 +1,36 @@
-let message:string = 'hello typescript course'
-console.log(message)
+// let message:string = 'hello typescript course'
+// console.log(message)
+
+let myName:string = "saeed sharifi"
+let score:number = 20
+let isProgrammer:boolean = true
+
+// incorrect data
+// let city:string = 50
+
+let numbers:number[] = [10,500,7000]
+let cities:Array<string> = ["Tehran", "shiraz", "rasht"]
+
+// incorrect data
+// let scores:number[] = [10,"1000", 5000]
+
+// tople array
+let person:[string, number] = ["amir", 33] 
+
+// object
+let info:{name:string, age:number} = {name: 'tara', age:20}
+
+// any (not recommended)
+let randomValue:any
+randomValue = "hello"
+randomValue = 55
+
+let text = "hello"
+// incorrect
+// text = 55
+
+
+// any
+let data
+data = 'hello'
+data = 100
